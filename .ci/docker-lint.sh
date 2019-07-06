aff874444a0a80fff65cb9a5256e7731c4a0a755
ocular-d <sven@ocular-d.tech>
2019-07-06 16:20:54 +0200

    chore(setup): start with repo structure and setup

---
+++ .ci/docker-lint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+
+echo ""
+echo "--------------------------------"
+echo "Running Hadoolint on Dockerfiles"
+echo ""
+
+#find `pwd` -iname "Dockerfile" -printf "%h\n" | sort -u | while read i; do                                              
+find "$(pwd)" -iname "Dockerfile" -printf "%h\n" | sort -u | while read -r i; do
+	cd "$i" && pwd && docker run --rm -i hadolint/hadolint < Dockerfile
+done
+
--- checks/remark/examples/test.md
+++
@@ -1,5 +0,0 @@
-# Test
-
-This is a test file.
-
-##Test2
