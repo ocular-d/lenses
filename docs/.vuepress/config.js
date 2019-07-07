99e3fd825141d8537e0b767f733ba539946a15ab
ocular-d <sven@ocular-d.tech>
2019-07-07 18:03:58 +0200

    chore(setup): more setup work for docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'Lenses',
+  themeConfig: {
+    logo: '/ocl-logo.png',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+    ]
+  }
+}
