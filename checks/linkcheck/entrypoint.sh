22dced283fa3293a3cfae1cf68c2789820f15e91
ocular-d <sven@ocular-d.tech>
2019-07-17 14:06:27 +0200

    fix(markdown): only check md files

--- checks/linkcheck/entrypoint.sh
+++ checks/linkcheck/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 # Re-set permission to the `ocl` user
 # This avoids permission denied if the data volume is mounted by root
 chown -R ttd /srv
-exec su-exec ocl /usr/local/bin/linkcheck -c '200,301,302' -t 5 /srv/test
+exec su-exec ocl /usr/local/bin/linkcheck -c '200,301,302' -e 'md' -t 5 /srv/test
