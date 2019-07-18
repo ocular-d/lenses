13170080bcc2f7a6ca9ade8da3aee8190140d529
ocular-d <sven@ocular-d.tech>
2019-07-18 10:36:36 +0200

    fix(user): use the right user

--- checks/linkcheck/entrypoint.sh
+++ checks/linkcheck/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# Re-set permission to the `ocl` user
+# Re-set permission to the `ttd` user
 # This avoids permission denied if the data volume is mounted by root
-chown -R ttd /srv
-exec su-exec ocl /usr/local/bin/linkcheck -c '200,301,302' -e 'md' -t 5 /srv/test
+#chown -R ttd /srv
+exec su-exec ttd /usr/local/bin/linkcheck -c '200,301,302' -e 'md' -t 5 /srv/test
